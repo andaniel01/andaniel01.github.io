43cb5ce4ba4e21d445ef2a01f659542bafecb5f7
andaniel01 <daniel1098761@gmail.com>
2020-12-28 19:44:32 -0500

    updated website

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {Navbar, Nav, Container, Row, Col} from 'react-bootstrap'
 import profilePic from './daniel.JPG'
 import waterlooLogo from './waterlooLogo.jpg'
+import experience1logo from './experience1Logo.png'
 
 function App() {
   return (
@@ -68,10 +69,42 @@ function App() {
           </div>
           <hr></hr>
           <div className='experience-type'>Education</div>
-          <Row>
+          <br></br>
+          <Row style={{paddingBottom: '7.5%'}}>
             <Col><img className='experience-image' src={waterlooLogo}/></Col>
-            <Col><strong>Univeristy of Waterloo</strong></Col>
+            <Col>
+              <div>
+                <Row>
+                  <Col xs={8}><strong>University of Waterloo</strong></Col>
+                  <Col>2019 - Present</Col>
+                </Row>
+              </div>
+              <div>Candidate for BASc Computer Engineering</div>
+              <br></br>
+              <p>I am currently in my 2nd year studying computer engineering. So far, I love the experiences I've gained and the memories 
+                I've made here so far. Although it may be hard sometimes, it feels great at the end when I see that I passed.  </p>
+            </Col>
           </Row>
+          <hr></hr>
+          <div className='experience-type'>Career</div>
+          <br></br>
+          <Row>
+            <Col><img className='experience-image' src={experience1logo}/></Col>
+            <Col>
+              <div>
+                <Row>
+                  <Col xs={8}><strong>Ministry of Children, Community, and Social Services</strong></Col>
+                  <Col>2019 - Present</Col>
+                </Row>
+              </div>
+              <div>Software Engineer</div>
+              <br></br>
+              <p>I am currently in my 2nd year studying computer engineering. So far, I love the experiences I've gained and the memories 
+                I've made here so far. Although it may be hard sometimes, it feels great at the end when I see that I passed.  </p>
+            </Col>
+          </Row>
+
+
         </Container>
       </div>
 

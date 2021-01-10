1eea0fae5d70bbfa76e270fba3788c921a5b99ad
andaniel01 <daniel1098761@gmail.com>
2021-01-10 02:51:21 -0500

    change name

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       <Navbar className='nav-bg-color' variant="light" expand="lg">
         <Container>
         <Navbar.Brand href="#home">
-          <div className='nav-text'>Daniel An</div>
+          <div className='nav-text'>Daniel Aniel</div>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
